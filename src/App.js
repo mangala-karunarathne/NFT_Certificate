e8e31413a6794b68ce8ebdaddde2e65ff3469023
User <Mangala Karunarathne>
2022-10-08 10:40:21 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import {BrowserRouter, Route, Router, Routes} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>NFT Certificate</h1>
+      <BrowserRouter> 
+          <Routes>
+            <Route path="/login" element={<login />} />
+          </Routes> 
+      </BrowserRouter>
     </div>
   );
 }
